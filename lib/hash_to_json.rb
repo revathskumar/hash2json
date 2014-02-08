7988c86f5e6090a05c98d3b6675ff71f8a937b61
Revath S Kumar <rsk@revathskumar.com>
2014-02-08 15:44:48 +0530

    Adding Ace editor
    * remove escaping backslash

--- lib/hash_to_json.rb
+++ lib/hash_to_json.rb
@@ -8,16 +8,38 @@ Cuba.define do
 
   on get do
     on root do
-      hash = ''
+      input = hash = ''
       on param('q') do | hash |
-        json = if hash && hash[ /=>/ ]
-          JSON.parse(hash.gsub('nil', '').gsub('=>', ' : ')).to_json
+        input = hash
+        old = hash[ /=>/ ]
+        # hash = hash.to_s.stringify_keys
+        # if old
+          # hash = Hash[hash.to_s.gsub('{', '').gsub('}', '').split(",").collect{|x|
+          #     x.gsub(/:| |\'/,'').gsub('nil', 'null').split("=>")
+          # }]
+          # p hash
+          # hash = hash.stringify_keys
+          # p hash
+        # end
+        json = if hash && old
+          JSON.parse(hash.to_s.gsub('=>', ':')).to_json
         else
           hash
         end
-        res.write render('templates/home.haml', hash: hash , json: json)
+        hash.gsub!('\\','')
+        res.write render('templates/home.haml', hash: input , json: json)
       end
       res.write render('templates/home.haml', hash: '' , json: '')
     end
   end
 end
+
+
+# class Hash
+#   def stringify_keys
+#     t = self.dup
+#     self.clear
+#     t.each_pair{|k, v| self[k.to_s] = v}
+#     self
+#   end
+# end
